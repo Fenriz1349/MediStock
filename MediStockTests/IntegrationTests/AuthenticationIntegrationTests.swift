22c2cee99407b0203d7dfc17731c4bdbb69c71d2
Julien Cotte <julien.cotte-cic@ibm.com>
2026-08-14 11:41:45 +0200

    test : add tests

--- MediStockTests/IntegrationTests/AuthenticationIntegrationTests.swift
+++ MediStockTests/IntegrationTests/AuthenticationIntegrationTests.swift
@@ -5,4 +5,35 @@
 //  Created by Julien Cotte on 14/08/2026.
 //
 
-import Foundation
+import XCTest
+@testable import MediStock
+
+final class AuthenticationIntegrationTests: XCTestCase {
+    @MainActor
+    func testScenario_signUpThenSignOutThenWrongPasswordThenCorrectPassword() async {
+        let service = AuthenticationServicingDouble()
+        let user = TestHelper.makeAppUser(email: "test@example.com")
+        service.signUpResult = .success(user)
+        let viewModel = TestHelper.makeAuthenticationViewModel(authenticationService: service)
+
+        viewModel.email = "test@example.com"
+        viewModel.password = "Password1!"
+        await viewModel.signUp()
+        XCTAssertEqual(viewModel.session, user)
+
+        viewModel.signOut()
+        XCTAssertNil(viewModel.session)
+
+        service.signInResult = .failure(AuthenticationError.wrongCredentials)
+        viewModel.password = "wrongPassword"
+        await viewModel.signIn()
+        XCTAssertNil(viewModel.session)
+        XCTAssertEqual(viewModel.error, .wrongCredentials)
+
+        service.signInResult = .success(user)
+        viewModel.password = "Password1!"
+        await viewModel.signIn()
+        XCTAssertEqual(viewModel.session, user)
+        XCTAssertNil(viewModel.error)
+    }
+}
--- MediStockTests/IntegrationTests/MedicineFormIntegrationTests.swift
+++ MediStockTests/IntegrationTests/MedicineFormIntegrationTests.swift
@@ -5,4 +5,36 @@
 //  Created by Julien Cotte on 14/08/2026.
 //
 
-import Foundation
+import XCTest
+@testable import MediStock
+
+final class MedicineFormIntegrationTests: XCTestCase {
+    @MainActor
+    func testScenario_createThenEdit_recordsAdditionThenUpdateWithPreviousValues() async {
+        let medicineStore = MedicineStoringDouble()
+        let historyStore = HistoryStoringDouble()
+        let createViewModel = TestHelper.makeMedicineFormViewModel(medicineStore: medicineStore,
+                                                                    historyStore: historyStore)
+        createViewModel.name = "Doliprane"
+        createViewModel.stockText = "10"
+
+        let created = await createViewModel.save(cleanedAisle: "AD56")
+        guard let created else {
+            XCTFail("Expected the medicine to be created")
+            return
+        }
+
+        let editViewModel = TestHelper.makeMedicineFormViewModel(existingMedicine: created,
+                                                                  medicineStore: medicineStore,
+                                                                  historyStore: historyStore)
+        editViewModel.name = "Dafalgan"
+        _ = await editViewModel.save(cleanedAisle: "AD10")
+
+        XCTAssertEqual(historyStore.addedMedicines.count, 1)
+        XCTAssertEqual(historyStore.updatedMedicines.count, 1)
+        XCTAssertEqual(historyStore.updateDetails.first?.previousName, "Doliprane")
+        XCTAssertEqual(historyStore.updateDetails.first?.previousAisle, "AD56")
+        XCTAssertEqual(medicineStore.savedMedicines.last?.name, "Dafalgan")
+        XCTAssertEqual(medicineStore.savedMedicines.last?.aisle, "AD10")
+    }
+}
